eb997ec44f513259fdb6a96473c24985e998fdb1
Sandeep Kulkarni <sandeep@snk.local>
2017-10-24 20:45:09 +0530

    Demo Ant Java project.
    It uses BigFraction class from org.apache.commons.math3.fraction to show
    its use.

---
+++ src/local/snk/BigFractionDemo.java
@@ -0,0 +1,22 @@
+package local.snk;
+
+import org.apache.commons.math3.fraction.*;
+
+public class BigFractionDemo {
+
+	/**
+	 * This is main method.
+	 * It uses BigFraction class from org.apache.commons.math3.fraction to show its use. 
+	 * @param args Input argument for the program.
+	 */
+	public static void main(String[] args) {
+		BigFraction one = new BigFraction(1524, 1000);
+        System.out.println("one   = " + one);
+
+        BigFraction two = new BigFraction(251, 100);
+        System.out.println("two   = " + two);
+
+        BigFraction three = one.divide(two);
+        System.out.println("three = " + three);
+	}
+}
